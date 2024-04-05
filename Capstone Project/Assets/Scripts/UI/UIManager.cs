a6222b1a399679f8eff656692008dc9ab23860e9
ChaseGivelekian <mc_cgivelekian25@mcjvs2.org>
2024-04-05 08:58:26 -0400

    Added the pause menu (need to fix the decimal formatting with the volume slider)

--- Capstone Project/Assets/Scripts/UI/UIManager.cs
+++ Capstone Project/Assets/Scripts/UI/UIManager.cs
@@ -6,10 +6,23 @@ public class UIManager : MonoBehaviour
     [SerializeField] private GameObject gameOverScreen;
     [SerializeField] private AudioClip gameOverSound;
 
+    [Header("Pause")]
+    [SerializeField] private GameObject pauseScreen;
+
     private void Awake()
     {
         gameOverScreen.SetActive(false);
+        pauseScreen.SetActive(false);
     }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //If pause screen already active unpause and viceversa
+            PauseGame(!pauseScreen.activeInHierarchy);
+        }
+    }
+    #region  Game Over
     //Activate game over screen
     public void GameOver()
     {
@@ -28,6 +41,35 @@ public class UIManager : MonoBehaviour
     public void Quit()
     {
         Application.Quit(); //Quits the game (only works in build)
+#if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false; //Exits play mode
+#endif
     }
+    #endregion
+
+    #region Pause
+    public void PauseGame(bool status)
+    {
+        //if status == true pause | if status == false unpause
+        pauseScreen.SetActive(status);
+
+        //When puase status is true change timescale to 0 (time stops) and when it's false change it back to 1 (time resumes)
+        if (status)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+    public void SoundVolume()
+    {
+        SoundManager.instance.ChangeSoundVolume(.1f);
+    }
+    public void MusicVolume()
+    {
+        SoundManager.instance.ChangeMusicVolume(.1f);
+    }
+    #endregion
 }
