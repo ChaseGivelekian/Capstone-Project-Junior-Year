e48c0ee74430c63087d02fd024611573c4fc15b4
ChaseGivelekian <mc_cgivelekian25@mcjvs2.org>
2024-04-30 10:12:37 -0400

    redid the main character model. need to work on melee attacks cause those are broke then adding right clicking for ranged attacks.

--- Capstone Project/Assets/Scripts/Player/PlayerAttack.cs
+++ Capstone Project/Assets/Scripts/Player/PlayerAttack.cs
@@ -31,8 +31,8 @@ public class PlayerAttack : MonoBehaviour
         anim.SetTrigger("attack");
         cooldownTimer = 0;
 
-        fireballs[FindFireball()].transform.position = firePoint.position;
-        fireballs[FindFireball()].GetComponent<Projectile>().SetDirection(Mathf.Sign(transform.localScale.x));
+        // fireballs[FindFireball()].transform.position = firePoint.position;
+        // fireballs[FindFireball()].GetComponent<Projectile>().SetDirection(Mathf.Sign(transform.localScale.x));
     }
     private int FindFireball()
     {
