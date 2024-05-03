82b08d691bf7985c54dd81aff792777257b2f8eb
ChaseGivelekian <mc_cgivelekian25@mcjvs2.org>
2024-05-03 10:29:48 -0400

    I can't believe it works

--- Capstone Project/Assets/Scripts/Player/PlayerFloating.cs
+++ Capstone Project/Assets/Scripts/Player/PlayerFloating.cs
@@ -2,24 +2,11 @@ using UnityEngine;
 
 public class PlayerFloating : MonoBehaviour
 {
-    // public float speed = .01f; // Speed at which the object moves up
-    private Animator anim;
     [SerializeField] public Transform target;
-    private float speed = 1f;
-    private Vector2 velocity = Vector2.zero;
-
-    private void Awake()
-    {
-        anim = GetComponent<Animator>();
-
-    }
     public void Update()
     {
-
-
         if (transform.position.y >= 5.3)
         {
-            // transform.position = new Vector2(target.position.x, 10);
             enabled = false;
         }
         else
@@ -28,10 +15,7 @@ public class PlayerFloating : MonoBehaviour
             target.GetComponent<PlayerMovement>().enabled = false;
             target.GetComponent<Rigidbody2D>().gravityScale = 0;
 
-            // Vector2 targetPosition = new Vector2(target.position.x, 10);
-            // transform.position = Vector2.SmoothDamp(transform.position, targetPosition, ref velocity, speed);
             transform.Translate(Vector3.up * (Time.deltaTime * 7));
         }
-        Debug.Log("I hope this stops");
     }
 }
