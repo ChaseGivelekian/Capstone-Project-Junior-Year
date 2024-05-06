6c69b72b83fbb538772acea4a14cfde46bd229fb
ChaseGivelekian <mc_cgivelekian25@mcjvs2.org>
2024-05-06 09:13:55 -0400

    fixed the falling through the ground bug

--- Capstone Project/Assets/Scripts/Player/PlayerFloating.cs
+++ Capstone Project/Assets/Scripts/Player/PlayerFloating.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 public class PlayerFloating : MonoBehaviour
 {
     [SerializeField] public Transform target;
+    private Rigidbody2D body;
+
+    public void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
     public void Update()
     {
         if (transform.position.y >= 5.3)
@@ -14,6 +20,7 @@ public class PlayerFloating : MonoBehaviour
             target.GetComponent<BoxCollider2D>().enabled = false;
             target.GetComponent<PlayerMovement>().enabled = false;
             target.GetComponent<Rigidbody2D>().gravityScale = 0;
+            body.velocity = Vector2.zero;
 
             transform.Translate(Vector3.up * (Time.deltaTime * 7));
         }
