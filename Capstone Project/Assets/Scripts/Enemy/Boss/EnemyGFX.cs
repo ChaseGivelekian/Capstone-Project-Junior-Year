65ad4a4a67116de1850cc5236bb637e1ff79e136
ChaseGivelekian <mc_cgivelekian25@mcjvs2.org>
2024-05-12 14:57:39 -0400

    working on the boss level. in progress

--- Capstone Project/Assets/Scripts/Enemy/Boss/EnemyGFX.cs
+++ Capstone Project/Assets/Scripts/Enemy/Boss/EnemyGFX.cs
@@ -3,10 +3,33 @@ using Pathfinding;
 
 public class EnemyGFX : MonoBehaviour
 {
+    [SerializeField] private Transform enemy;
+    [SerializeField] private Rigidbody2D rb;
+    public float speed = 200f;
+    public float jumpNodeHeightRequirement = .8f;
+    public float jumpModifier = .3f;
     public AIPath aiPath;
+    public float jumpCheckOffset;
+    private bool isGrounded = false;
 
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
+        enemy = enemy.GetComponent<Transform>();
+        isGrounded = Physics2D.Raycast(transform.position, -Vector3.up, GetComponent<Collider2D>().bounds.extents.y + jumpCheckOffset);
+        Debug.Log(isGrounded);
+        if (isGrounded)
+        {
+            if (enemy.position.y > jumpNodeHeightRequirement)
+            {
+                rb.AddForce(Vector2.up * speed * jumpModifier);
+            }
+        }
+
         if (aiPath.desiredVelocity.x >= .01f)
         {
             transform.localScale = new Vector3(3, 3, 3);
